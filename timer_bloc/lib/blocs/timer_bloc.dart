37801627de49a8e8f9cd79ff07075944b436f070
Samuel <oluwatomisinadekunle@gmail.com>
2019-10-15 12:03:12 +0100

    Timer bloc

---
+++ timer_bloc/lib/blocs/timer_bloc.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:timer_bloc/data/ticker.dart';
+import 'package:timer_bloc/blocs/states/timer_state.dart';
+import 'package:timer_bloc/blocs/events/timer_event.dart';
+
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  final Ticker _ticker;
+  final int _duration = 60;
+
+  StreamSubscription _tickerSubscription;
+
+  TimerBloc({
+    Ticker ticker,
+  }) : assert(ticker != null),
+        _ticker = ticker;
+
+  @override
+  TimerState get initialState => Ready(_duration);
+
+  @override
+  Stream<TimerState> mapEventToState(TimerEvent event) async* {
+    if (event is Start) {
+      yield* _mapStartToState(event);
+    } else if (event is Resume) {
+      yield* _mapResumeToState(event);
+    } else if (event is Pause) {
+      yield* _mapPauseToState(event);
+    } else if (event is Reset) {
+      yield* _mapResetToState(event);
+    } else if (event is Tick) {
+      yield* _mapTickToState(event);
+    }
+  }
+
+  @override
+  void dispose() {
+    _tickerSubscription?.cancel();
+    super.dispose();
+  }
+
+  Stream<TimerState> _mapStartToState(Start start) async* {
+    yield Running(start.duration);
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker.tick(ticks: start.duration).listen((int duration) {
+      dispatch(Tick(duration: duration));
+    });
+  }
+
+  Stream<TimerState> _mapResumeToState(Resume resume) async* {
+    if (currentState is Resume) {
+      _tickerSubscription?.resume();
+      yield Running(currentState.duration);
+    }
+  }
+
+  Stream<TimerState> _mapPauseToState(Pause pause) async* {
+    if (currentState is Running) {
+      _tickerSubscription?.pause();
+      yield Paused(currentState.duration);
+    }
+  }
+
+  Stream<TimerState> _mapResetToState(Reset reset) async* {
+    _tickerSubscription?.cancel();
+    yield Ready(currentState.duration);
+  }
+
+  Stream<TimerState> _mapTickToState(Tick tick) async* {
+    yield tick.duration > 0 ? Running(tick.duration) : Finished();
+  }
+}
