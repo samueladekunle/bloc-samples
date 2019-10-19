9e1c6015fe8822ed3f15024578bc58d45facbf83
Samuel <oluwatomisinadekunle@gmail.com>
2019-10-19 21:10:24 +0100

    Post event

---
+++ infinite_list_bloc/lib/blocs/post_event.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+abstract class PostEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class Fetch extends PostEvent {
+  @override
+  String toString() => 'Fetch';
+}
